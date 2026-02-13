1f7471765405d11f46adf07a33bc87734e8231ee
Sanumong-void <marmasunmoon9@gmail.com>
2026-02-13 20:13:40 +0600

    Update code_From_Scratch_&_PyTorch.py

--- Week 01/code_From_Scratch_&_PyTorch.py
+++ Week 01/code_From_Scratch_&_PyTorch.py
@@ -1,3 +1,4 @@
+# Using Numpy Alone
 
 import numpy as np
 def relu(z):
@@ -26,19 +27,16 @@ b2 = np.zeros((1, output_size))
 
 for epoch in range(1000):
 
-    # ----- Forward pass -----
     z1 = X @ W1 + b1
     a1 = relu(z1)
 
     z2 = a1 @ W2 + b2
     y_hat = sigmoid(z2)
 
-    # ----- Loss (Binary Cross-Entropy) -----
     loss = -np.mean(y * np.log(y_hat + 1e-8) +                  # 1e-8 is used to prevent log(0) explosion 
                      (1 - y) * np.log(1 - y_hat + 1e-8))        # But professional way,Clip y_hat to be between 1e-8 and 0.99999999, 
-                                                                # y_hat_clipped = np.clip(y_hat, 1e-8, 1 - 1e-8)
-                                                                # loss = -np.mean(y * np.log(y_hat_clipped) + (1 - y) * np.log(1 - y_hat_clipped))
-    # ----- Backpropagation -----
+                                                                # y_hat_clipped = np.clip(y_hat, 1e-8, 1 - 1e-8)                                                     
+                                                                # loss = -np.mean(y * np.log(y_hat_clipped) + (1 - y) * np.log(1 - y_hat_clipped)
     dz2 = y_hat - y
     dW2 = a1.T @ dz2 / len(X)
     db2 = np.mean(dz2, axis=0, keepdims=True)
@@ -48,7 +46,6 @@ for epoch in range(1000):
     dW1 = X.T @ dz1 / len(X)
     db1 = np.mean(dz1, axis=0, keepdims=True)
 
-    # ----- Update -----
     W2 -= lr * dW2
     b2 -= lr * db2
     W1 -= lr * dW1
@@ -56,24 +53,50 @@ for epoch in range(1000):
 
     if epoch % 200 == 0:
         print(f"Epoch {epoch}, Loss: {loss:.4f}")
-# --- PREDICTION FOR A NEW USER ---
 
-# 1. Get input from the user
+
 val1 = float(input("Enter first feature: "))
 val2 = float(input("Enter second feature: "))
 
-# 2. Put it in the right shape (1 row, 2 columns)
-# This matches the shape the model expects
 user_input = np.array([[val1, val2]])
 
-# 3. The Forward Pass (Just the math, no learning here)
 layer1_raw = user_input @ W1 + b1
-layer1_act = np.maximum(0, layer1_raw)  # ReLU
+layer1_act = np.maximum(0, layer1_raw)  
 layer2_raw = layer1_act @ W2 + b2
-prediction = 1 / (1 + np.exp(-layer2_raw)) # Sigmoid
+prediction = 1 / (1 + np.exp(-layer2_raw)) 
 
-# 4. Turn the decimal into a 0 or 1
 final_choice = 1 if prediction > 0.5 else 0
 
 print(f"Model Probability: {prediction[0,0]:.4f}")
 print(f"Model Prediction: {final_choice}")
+
+# Using PyTorch
+
+import torch
+import torch.nn as nn
+
+torch.manual_seed(0)
+
+
+model = nn.Sequential(
+    nn.Linear(2, 5),
+    nn.ReLU(),
+    nn.Linear(5, 1),
+    nn.Sigmoid()
+)
+
+
+X = torch.randn(10, 2)
+y = (X[:, 0] + X[:, 1] > 0).float().unsqueeze(1)
+
+
+criterion = nn.BCELoss()
+
+
+y_hat = model(X)
+loss = criterion(y_hat, y)
+
+
+model.zero_grad()    
+loss.backward()       
+
